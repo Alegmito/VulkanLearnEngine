6754458b6e25b26416b4c98e985997c613b9dc0f
Daniil Smirnov <daniil.smirnov007@gmail.com>
2025-09-05 19:38:03 +0300

    WIP: Add command buffer

--- src/vulkan_app.cpp
+++ src/vulkan_app.cpp
@@ -48,6 +48,42 @@ void VulkanApp::createPipeline() {
 }
 
 void VulkanApp::createCommandBuffers() {
+    commandBuffers_.resize(swapChain_.imageCount());
+
+    VkCommandBufferAllocateInfo allocInfo {};
+    allocInfo.sType = VK_STRUCTURE_TYPE_COMMAND_BUFFER_ALLOCATE_INFO;
+    allocInfo.level = VK_COMMAND_BUFFER_LEVEL_PRIMARY;
+
+    if (vkAllocateCommandBuffers(device_.getDevice(), &allocInfo, commandBuffers_.data()) != VK_SUCCESS) {
+        throw std::runtime_error("failed to allocate command buffers");
+    }
+
+    size_t i {};
+    for (auto& commandBuffer : commandBuffers_) {
+        VkCommandBufferBeginInfo commandBuffInfo {};
+        commandBuffInfo.sType = VK_STRUCTURE_TYPE_COMMAND_BUFFER_BEGIN_INFO;
+
+        if (vkBeginCommandBuffer(commandBuffer, &commandBuffInfo) != VK_SUCCESS) {
+            throw std::runtime_error("failed to begib recording command buffer");
+        }
+
+        VkRenderPassBeginInfo renderPassInfo {};
+        renderPassInfo.sType = VK_STRUCTURE_TYPE_RENDER_PASS_BEGIN_INFO;
+        renderPassInfo.renderPass = swapChain_.getRenderPass();
+        renderPassInfo.framebuffer = swapChain_.getFrameBuffer(i);
+
+        renderPassInfo.renderArea.offset = {0, 0};
+        renderPassInfo.renderArea.extent = swapChain_.getSwapChainExtent();
+
+        std::vector<VkClearValue> clearValues{};
+        clearValues[0].color = {0.1f, 0.1f, 0.1f, 0.1f};
+        clearValues[1].depthStencil = {1.0, 0};
+        renderPassInfo.clearValueCount = clearValues.size();
+        renderPassInfo.pClearValues = clearValues.data();
+
+        vkCmdBeginRenderPass(commandBuffer, &renderPassInfo, VK_SUBPASS_CONTENTS_INLINE);
+        ++i;
+    }
 }
 
 void VulkanApp::drawFrame() {
